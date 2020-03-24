ad2b25415d6e371bbc361378e98d42533493d273
Jan Jakub Nanista <jan.jakub.nanista@gmail.com>
2020-03-24 22:24:16 +0100

    First draft of the transformer

---
+++ transformer/index.ts
@@ -0,0 +1,198 @@
+import {
+  FunctionExpression,
+  Identifier,
+  Program,
+  PropertyAssignment,
+  SourceFile,
+  SyntaxKind,
+  TransformationContext,
+  TransformerFactory,
+  Type,
+  TypeFlags,
+  createBlock,
+  createCall,
+  createFunctionExpression,
+  createIdentifier,
+  createLiteral,
+  createLogicalAnd,
+  createLogicalOr,
+  createNull,
+  createParameter,
+  createPropertyAccess,
+  createPropertyAssignment,
+  createReturn,
+  createStrictEquality,
+  createStrictInequality,
+  createStringLiteral,
+  createTypeOf,
+  isArrayTypeNode,
+  isMethodSignature,
+  isPropertyDeclaration,
+  isPropertySignature,
+} from 'typescript';
+import { ValueTypeCheckCreator, visitNodeAndChildren } from './visitor';
+import { addTypeCheckerMap, createTypeCheckerFunction } from './utils';
+
+interface TypeCheckMethod {
+  name: string;
+  definition: FunctionExpression;
+}
+
+export default (program: Program): TransformerFactory<SourceFile> => {
+  return (context: TransformationContext) => (file: SourceFile) => {
+    const typeChecker = program.getTypeChecker();
+    const typeCheckerObjectIdentfifier: Identifier = createIdentifier('__typeCheckerMap__');
+    const typeCheckMethods: Map<Type, TypeCheckMethod> = new Map();
+
+    // Main type check generator method
+    const createTypeCheckMethodDefinition = (type: Type): FunctionExpression => {
+      console.warn('type', type.symbol?.getName(), type.flags);
+
+      if (type.flags & TypeFlags.Boolean) {
+        return createTypeCheckerFunction(value =>
+          createStrictEquality(createTypeOf(value), createStringLiteral('boolean')),
+        );
+      }
+
+      if (type.flags & TypeFlags.BooleanLiteral) {
+        const typeNode = typeChecker.typeToTypeNode(type);
+
+        return createTypeCheckerFunction(value =>
+          createStrictEquality(value, createLiteral(typeNode?.kind === SyntaxKind.TrueKeyword)),
+        );
+      }
+
+      if (type.flags & TypeFlags.Number) {
+        return createTypeCheckerFunction(value =>
+          createStrictEquality(createTypeOf(value), createStringLiteral('number')),
+        );
+      }
+
+      if (type.flags & TypeFlags.String) {
+        return createTypeCheckerFunction(value =>
+          createStrictEquality(createTypeOf(value), createStringLiteral('string')),
+        );
+      }
+
+      if (type.flags & TypeFlags.Undefined) {
+        return createTypeCheckerFunction(value =>
+          createStrictEquality(createTypeOf(value), createStringLiteral('undefined')),
+        );
+      }
+
+      if (type.isLiteral()) {
+        return createTypeCheckerFunction(value => createStrictEquality(value, createLiteral(type.value)));
+      }
+
+      if (type.isUnion()) {
+        return createTypeCheckerFunction(value => {
+          return type.types
+            .map(unionMemberType => {
+              return createValueTypeCheck(unionMemberType, value);
+            })
+            .reduce((expression, unionMemberExpression) => createLogicalOr(expression, unionMemberExpression));
+        });
+      }
+
+      const typeNode = typeChecker.typeToTypeNode(type);
+      if (typeNode && isArrayTypeNode(typeNode)) {
+        const indexType = type.getNumberIndexType();
+        if (!indexType) {
+          throw new Error('Could not find array type for ' + typeNode.getFullText());
+        }
+
+        return createTypeCheckerFunction(value => {
+          const isArray = createCall(createPropertyAccess(createIdentifier('Array'), 'isArray'), [], [value]);
+          const element = createIdentifier('element');
+          const checkElement = createFunctionExpression(
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            [
+              createParameter(
+                /* decorators */ undefined,
+                /* modifiers */ undefined,
+                /* dotDotDotToken */ undefined,
+                /* name */ element,
+                undefined,
+                undefined,
+                undefined,
+              ),
+            ],
+            undefined,
+            createBlock([createReturn(createValueTypeCheck(indexType, element))], false),
+          );
+          const checkElements = createCall(createPropertyAccess(value, 'every'), [], [checkElement]);
+
+          return createLogicalAnd(isArray, checkElements);
+        });
+      }
+
+      return createTypeCheckerFunction(value => {
+        const isObject = createLogicalAnd(
+          createStrictEquality(createTypeOf(value), createStringLiteral('object')),
+          createStrictInequality(value, createNull()),
+        );
+
+        return type.getProperties().reduce((expression, property) => {
+          const declaration = property.valueDeclaration;
+          // TODO Properties without a type, just an initializer
+          if (
+            !isPropertySignature(declaration) &&
+            !isMethodSignature(declaration) &&
+            !isPropertyDeclaration(declaration)
+          ) {
+            console.warn('declaration', declaration.kind);
+
+            throw new Error(`Property ${property.name} does not have a property declaration`);
+          }
+
+          if (!declaration.type) {
+            throw new Error(`Could not determine the type of property ${property.name}`);
+          }
+
+          const type = typeChecker.getTypeFromTypeNode(declaration.type);
+          return createLogicalAnd(expression, createValueTypeCheck(type, createPropertyAccess(value, property.name)));
+        }, isObject);
+      });
+    };
+
+    let lastMethodId = 0;
+    const createTypeCheckMethod = (type: Type): TypeCheckMethod => {
+      const name = `__${lastMethodId++}`;
+      const defaultMethod = { name, definition: undefined };
+
+      // FIXME We first need to mark this node, then create its definition to prevent recursion
+      typeCheckMethods.set(type, defaultMethod as any);
+
+      const definition = createTypeCheckMethodDefinition(type);
+      const method = { name, definition };
+      typeCheckMethods.set(type, method);
+
+      return method;
+    };
+
+    const createValueTypeCheck: ValueTypeCheckCreator = (type, value) => {
+      const method = typeCheckMethods.get(type) || createTypeCheckMethod(type);
+
+      return createCall(
+        createPropertyAccess(typeCheckerObjectIdentfifier, method.name),
+        /* typeArguments */ undefined,
+        /* arguments */ [value],
+      );
+    };
+
+    const transformedFile = visitNodeAndChildren(file, program, context, createValueTypeCheck);
+    const typeCheckerProperties: PropertyAssignment[] = Array.from(typeCheckMethods.values()).map(method => {
+      return createPropertyAssignment(method.name, method.definition);
+    });
+    const transformedFileWithTypeCheckerMap = addTypeCheckerMap(
+      transformedFile,
+      typeCheckerObjectIdentfifier,
+      typeCheckerProperties,
+    );
+
+    return transformedFileWithTypeCheckerMap;
+  };
+};
---
+++ transformer/visitor.ts
@@ -0,0 +1,56 @@
+import { Expression, Node, Program, SourceFile, TransformationContext, Type, visitEachChild } from 'typescript';
+import { isOurCallExpression, isOurImportExpression } from './utils';
+
+export type ValueTypeCheckCreator = (type: Type, value: Expression) => Expression;
+
+function visitNode(node: SourceFile, program: Program, typeCheckValue: ValueTypeCheckCreator): SourceFile;
+function visitNode(node: Node, program: Program, typeCheckValue: ValueTypeCheckCreator): Node | undefined;
+function visitNode(node: Node, program: Program, typeCheckValue: ValueTypeCheckCreator): Node | undefined {
+  // Discard all the imports from this module
+  if (isOurImportExpression(node)) {
+    return;
+  }
+
+  const typeChecker = program.getTypeChecker();
+  if (isOurCallExpression(node, 'isA', typeChecker)) {
+    const typeNode = node.typeArguments?.[0];
+    if (!typeNode) {
+      throw new Error('isA<T>() requires one type parameter, none specified');
+    }
+
+    const valueNode = node.arguments[0];
+    if (!valueNode) {
+      throw new Error('isA<T>() requires one argument, none specified');
+    }
+
+    const type = typeChecker.getTypeFromTypeNode(typeNode);
+    return typeCheckValue(type, valueNode);
+  }
+
+  return node;
+}
+
+export function visitNodeAndChildren(
+  node: SourceFile,
+  program: Program,
+  context: TransformationContext,
+  typeCheckValue: ValueTypeCheckCreator,
+): SourceFile;
+export function visitNodeAndChildren(
+  node: Node,
+  program: Program,
+  context: TransformationContext,
+  typeCheckValue: ValueTypeCheckCreator,
+): Node | undefined;
+export function visitNodeAndChildren(
+  node: Node,
+  program: Program,
+  context: TransformationContext,
+  typeCheckValue: ValueTypeCheckCreator,
+): Node | undefined {
+  return visitEachChild(
+    visitNode(node, program, typeCheckValue),
+    childNode => visitNodeAndChildren(childNode, program, context, typeCheckValue),
+    context,
+  );
+}
