cd7d40a284c6c16229e1e5b3c03d69abce0f3222
Jan Jakub Nanista <jan.jakub.nanista@gmail.com>
2020-04-12 22:14:47 +0200

    WIP

--- transformer/index.ts
+++ transformer/index.ts
@@ -26,190 +26,13 @@ export default (program: Program): TransformerFactory<SourceFile> => {
     const typeCheckerObjectIdentfifier: Identifier = createIdentifier('__typeCheckerMap__');
     const typeCheckMethods: Map<ts.Type, TypeCheckMethod> = new Map();
 
+    // I believe this is the only hack in the whole codebase
+    //
+    // This API is marked as internal in TypeScript compiler
     const isArrayType = (type: ts.Type): boolean => (typeChecker as any).isArrayType?.(type) || false;
 
-    // // Main type check generator method
-    // const createTypeCheckMethodDefinition = (typeNode: TypeNode): FunctionExpression => {
-    //   if (ts.isTypeReferenceNode(typeNode) || ts.isTypeLiteralNode(typeNode)) {
-    //     return createTypeCheckerFunction(value => {
-    //       // First we check whether the thing is an object
-    //       const isObject = createIsPlainObjectCheck(value);
-    //       const explicitPropertyChecks = createObjectPropertiesCheck(
-    //         typeChecker,
-    //         createValueTypeCheck,
-    //         typeNode,
-    //         value,
-    //       );
-    //       const indexedPropertyChecks = createObjectIndexedPropertiesCheck(
-    //         typeChecker,
-    //         createValueTypeCheck,
-    //         typeNode,
-    //         value,
-    //       );
-
-    //       if (explicitPropertyChecks && indexedPropertyChecks)
-    //         return ts.createLogicalAnd(
-    //           ts.createParen(isObject),
-    //           ts.createLogicalAnd(ts.createParen(explicitPropertyChecks), ts.createParen(indexedPropertyChecks)),
-    //         );
-
-    //       if (explicitPropertyChecks)
-    //         return ts.createLogicalAnd(ts.createParen(isObject), ts.createParen(explicitPropertyChecks));
-
-    //       if (indexedPropertyChecks)
-    //         return ts.createLogicalAnd(ts.createParen(isObject), ts.createParen(indexedPropertyChecks));
-
-    //       return isObject;
-    //     });
-    //   }
-
-    //   return createTypeCheckerFunction(() => createFalse());
-    // };
-
-    // let lastMethodId = 0;
-    // const createTypeCheckMethod = (typeNode: TypeNode): TypeCheckMethod => {
-    //   const name = `__${lastMethodId++}`;
-    //   const defaultMethod = { name, definition: undefined };
-
-    //   // FIXME We first need to mark this node, then create its definition to prevent recursion
-    //   typeCheckMethods.set(typeNode, defaultMethod as any);
-
-    //   const definition = createTypeCheckMethodDefinition(typeNode);
-    //   const method = { name, definition };
-    //   typeCheckMethods.set(typeNode, method);
-
-    //   return method;
-    // };
-
-    // const createValueTypeCheck: ValueTypeCheckCreator = (typeNode, value, logger = createLogger()) => {
-    //   const type = typeChecker.getTypeFromTypeNode(typeNode);
-    //   logger('[createValueTypeCheck]', typeChecker.typeToString(type), typeNode.kind, type.flags);
-
-    //   const node = (type as ts.TypeReference).node;
-    //   logger('\tNode:', node?.kind);
-
-    //   // // FIXME Flags look like a bitmap rather than a number
-    //   // switch (type.flags) {
-    //   //   case ts.TypeFlags.Any:
-    //   //   case ts.TypeFlags.Unknown:
-    //   //     logger('\tis any');
-    //   //     break;
-
-    //   //   case ts.TypeFlags.String:
-    //   //   case ts.TypeFlags.Boolean:
-    //   //   case ts.TypeFlags.Number:
-    //   //     logger('\tis typeofable');
-    //   //     break;
-
-    //   //   case ts.TypeFlags.Intersection:
-    //   //     logger('\tis intersection');
-    //   //     break;
-
-    //   //   case ts.TypeFlags.Union:
-    //   //     logger('\tis union');
-    //   //     break;
-    //   // }
-
-    //   // Checks for any/unknown types
-    //   if (hasNoConstraint(typeNode)) {
-    //     return ts.createTrue();
-    //   }
-
-    //   // Checks for types that can be asserted using the "typeof" keyword
-    //   const typeOfNode = getTypeOf(typeNode);
-    //   if (typeOfNode) {
-    //     return ts.createStrictEquality(ts.createTypeOf(value), ts.createStringLiteral(typeOfNode));
-    //   }
-
-    //   // Checks for any kind of literal
-    //   // - true / false
-    //   // - null
-    //   // - undefined
-    //   // - "string literal"
-    //   const literal = getLiteral(typeNode);
-    //   if (literal) {
-    //     return ts.createStrictEquality(value, literal);
-    //   }
-
-    //   // Check for an array of any kind (the element type check is created by calling )
-    //   if (ts.isArrayTypeNode(typeNode)) {
-    //     return createArrayElementsCheck(typeChecker, createValueTypeCheck, typeNode, value);
-    //   }
-
-    //   if (ts.isTypeReferenceNode(typeNode)) {
-    //     logger('\tType reference node');
-    //     if (node) {
-    //       if (ts.isArrayTypeNode(node)) {
-    //         logger('\t\tGot array', node.kind);
-    //         return createValueTypeCheck(node, value, logger.indent());
-    //       }
-
-    //       if (ts.isArrayTypeNode(node)) {
-    //         logger('\t\tGot tuple', node.kind);
-    //         // return createValueTypeCheck(node, value, logger.indent());
-    //       }
-
-    //       logger('\t\tGot uneexpected', node.kind);
-    //     }
-
-    //     // const node = (type as ts.TypeReference).node;
-    //     debugger;
-    //   }
-
-    //   if (ts.isTypeLiteralNode(typeNode)) {
-    //     logger('\tType literal node');
-    //   }
-
-    //   if (ts.isUnionTypeNode(typeNode)) {
-    //     logger('\tUnion type node');
-
-    //     return typeNode.types
-    //       .map(memberTypeNode => {
-    //         return createValueTypeCheck(memberTypeNode, value, logger.indent());
-    //       })
-    //       .reduce((expression, unionMemberExpression) => ts.createLogicalOr(expression, unionMemberExpression));
-    //   }
-
-    //   if (type.isUnion()) {
-    //     logger('\tUnion type');
-
-    //     return type.types
-    //       .map(unionMemberType => {
-    //         const unionMemberTypeNode = typeChecker.typeToTypeNode(unionMemberType, typeNode);
-    //         if (!unionMemberTypeNode) {
-    //           throw new Error(`Could not resolve union member type node`);
-    //         }
-
-    //         return createValueTypeCheck(unionMemberTypeNode, value, logger.indent());
-    //       })
-    //       .reduce((expression, unionMemberExpression) => ts.createLogicalOr(expression, unionMemberExpression));
-    //   }
-
-    //   // Now the ones that will
-    //   const method = typeCheckMethods.get(typeNode) || createTypeCheckMethod(typeNode);
-    //   return createCall(
-    //     ts.createPropertyAccess(typeCheckerObjectIdentfifier, method.name),
-    //     /* typeArguments */ undefined,
-    //     /* arguments */ [value],
-    //   );
-    // };
-
-    const logger = createLogger('[isACallVisitor]');
-
     let lastMethodId = 0;
-    const createTypeCheckMethod = (type: ts.Type): TypeCheckMethod => {
-      const name = `__${lastMethodId++}`;
-      const defaultMethod = { name, definition: undefined };
-
-      // FIXME We first need to mark this node, then create its definition to prevent recursion
-      typeCheckMethods.set(type, defaultMethod as any);
-
-      const definition = createTypeCheckMethodDefinition(typeNode);
-      const method = { name, definition };
-      typeCheckMethods.set(type, method);
-
-      return method;
-    };
+    const logger = createLogger('[isACallVisitor]');
 
     const createCheckForType = (root: ts.TypeNode, type: ts.Type, value: ts.Expression): ts.Expression => {
       const typeName = typeChecker.typeToString(type, root);
@@ -329,8 +152,6 @@ export default (program: Program): TransformerFactory<SourceFile> => {
 
         return ts.createTrue();
       }
-
-      logger('\tCould not find anything');
 
       // FIXME Throw an exception here
       return ts.createFalse();
