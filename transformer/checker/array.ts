b443aae35515b9d8fc10ee97458048e3be827858
Jan Jakub Nanista <jan.jakub.nanista@gmail.com>
2020-04-17 10:48:28 +0200

    WIP

--- transformer/checker/array.ts
+++ transformer/checker/array.ts
@@ -1,47 +1,20 @@
+import { createLogicalAndChain, createValueCheckFunction } from './utils';
 import ts from 'typescript';
 
-export type CreateArrayElementCheck = (value: ts.Expression, index: number | ts.Expression) => ts.Expression;
+export type CreateArrayElementCheck = (value: ts.Expression) => ts.Expression;
 
 export const createIsArray = (value: ts.Expression): ts.Expression =>
   ts.createCall(ts.createPropertyAccess(ts.createIdentifier('Array'), 'isArray'), [], [value]);
-
-export const createArrayElementsCheck = (
-  value: ts.Expression,
-  createElementCheck: CreateArrayElementCheck,
-): ts.Expression => {
-  const element = ts.createIdentifier('element');
-  const index = ts.createIdentifier('index');
-  const checkElement = ts.createArrowFunction(
-    undefined /* modifiers */,
-    undefined /* typeParameters */,
-    [
-      ts.createParameter(
-        undefined /* decorators */,
-        undefined /* modifiers */,
-        undefined /* dotDotDotToken */,
-        element /* name */,
-      ),
-      ts.createParameter(
-        undefined /* decorators */,
-        undefined /* modifiers */,
-        undefined /* dotDotDotToken */,
-        index /* name */,
-      ),
-    ],
-    undefined,
-    ts.createToken(ts.SyntaxKind.EqualsGreaterThanToken),
-    createElementCheck(element, index),
-  );
-
-  // Now let's do value.every(<element type checker>)
-  const checkElements = ts.createCall(ts.createPropertyAccess(value, 'every'), [], [checkElement]);
-
-  return ts.createLogicalAnd(createIsArray(value), checkElements);
-};
 
 export const createArrayTypeCheck = (
   value: ts.Expression,
   createElementCheck: CreateArrayElementCheck,
 ): ts.Expression => {
-  return ts.createLogicalAnd(createIsArray(value), createArrayElementsCheck(value, createElementCheck));
+  const elementChecks = ts.createCall(
+    ts.createPropertyAccess(value, 'every'),
+    [],
+    [createValueCheckFunction(createElementCheck)],
+  );
+
+  return createLogicalAndChain(createIsArray(value), elementChecks);
 };
--- transformer/checker/tuple.ts
+++ transformer/checker/tuple.ts
@@ -9,7 +9,7 @@ export const createTupleCheck = (
 ): ts.Expression => {
   const arrayLengthCheck = ts.createStrictEquality(ts.createPropertyAccess(value, 'length'), ts.createLiteral(length));
   const elementChecks = Array.from({ length }).map((_, index) =>
-    createElementCheck(ts.createElementAccess(value, index), index),
+    createElementCheck(ts.createElementAccess(value, index)),
   );
 
   return createLogicalAndChain(createIsArray(value), arrayLengthCheck, ...elementChecks);
