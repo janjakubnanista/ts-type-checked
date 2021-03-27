6aef4efdda9b95881092bb60d44e7fe94981c3bf
Ján Naništa <janjakub@fb.com>
2021-03-27 16:31:20 +0000

    Make sure strict mode is used also when strict: true is in the tsconfig.json

--- src/transformer/index.ts
+++ src/transformer/index.ts
@@ -33,7 +33,7 @@ export default (
 
   // Without strict null checks on we need to
   const compilerOptions = program.getCompilerOptions();
-  const strictNullChecks = !!compilerOptions.strictNullChecks;
+  const strictNullChecks = !!compilerOptions.strictNullChecks || !!compilerOptions.strict;
 
   // Get a reference to a TypeScript TypeChecker in order to resolve types from type nodes
   const typeChecker = program.getTypeChecker();
